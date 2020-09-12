860d7687e2241ae98bd885373b703127bbbd4951
Hayden Myers <haydenmyers@Haydens-MacBook-Pro.local>
2020-09-12 08:01:59 +0100

    Added travis.yml file

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('renders learn react link', () => {
+test('second test that should fail', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/this should be wrong/i);
   expect(linkElement).toBeInTheDocument();
